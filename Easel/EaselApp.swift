f5bec189928a34263fc7c824f3f754211e4bc483
Fadhli Rahim <fadrahim1@gmail.com>
2023-01-01 11:16:36 +0800

    Initial Commit

---
+++ Easel/EaselApp.swift
@@ -0,0 +1,17 @@
+//
+//  EaselApp.swift
+//  Easel
+//
+//  Created by Fadhli Rahim on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct EaselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
