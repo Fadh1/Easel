ffd8c2ee89556dd4a244ef21512c9840a68a6859
Fadhli Rahim <fadrahim1@gmail.com>
2023-01-01 11:22:44 +0800

    Second commit

--- Easel/ContentView.swift
+++ Easel/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hey there!")
+            Text("Hey there! Twice")
         }
         .padding()
     }
