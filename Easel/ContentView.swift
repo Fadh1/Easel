65797e2c1c3eb95d35147d7edaa0292b78c69370
Fadhli Rahim <fadrahim1@gmail.com>
2023-01-02 15:27:48 +0800

    Added in the PencilKit

--- Easel/ContentView.swift
+++ Easel/ContentView.swift
@@ -6,16 +6,19 @@
 //
 
 import SwiftUI
+import PencilKit
+
+/**
+ Goal here is to create a drawing app from where I am now.
+ First up, let's figure out the stuff on the sdie here
+ Okay pencil kit seems to be the
+ What is helpful is the idea of pencilKit
+ I think I just need to add pencilkit
+ */
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hey there! Twice")
-        }
-        .padding()
+        PencilKitView()
     }
 }
 
@@ -24,3 +27,29 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct PencilKitView: UIViewRepresentable {
+    typealias UIViewType = PKCanvasView
+    
+    let toolPicker = PKToolPicker()
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        let pencilKitCanvasView = PKCanvasView()
+        
+        pencilKitCanvasView.drawingPolicy = PKCanvasViewDrawingPolicy.anyInput
+        
+        toolPicker.addObserver(pencilKitCanvasView)
+        toolPicker.setVisible(true, forFirstResponder: pencilKitCanvasView)
+        
+        pencilKitCanvasView.becomeFirstResponder()
+        
+        return pencilKitCanvasView
+        
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context){
+        
+    }
+    
+}
+
