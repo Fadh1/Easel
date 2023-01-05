c7fd1ae8c21acf91711359b0fc4449dd93b25e42
Fadhli Rahim <fadrahim1@gmail.com>
2023-01-05 20:46:50 +0800

    Changed DetailView

--- Easel/DetailView.swift
+++ Easel/DetailView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct DetailView: View {
     let scrum : DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            Section(header: Text("Meeting Info")){
+                Label("Start Meeting", systemImage: "timer").font(.headline).foregroundColor(.accentColor)
+            }
+        }
+        
     }
 }
 
